3dcaf90fd7634ff3b6d722990a333f69c46aad1f
Max Kohl <maxhkohl@github.com>
2015-10-24 15:16:08 -0700

    Initial commit of TIN to Mesh function

---
+++ PNG2Mesh/Assets/CreateMesh.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateMesh : MonoBehaviour {
+	
+	public Material mat;
+	public string meshName;
+	public UnityEngine.Vector3 color;
+	public Vector3[] vertices;
+	public int[] triangles;
+	public int numVertices;
+	public int numTriangles;
+
+	public Mesh MeshSetup (string fileName)
+	{
+		string line;
+		// Read the file and display it line by line.
+		System.IO.StreamReader file = new System.IO.StreamReader(fileName);
+		
+		line = file.ReadLine();
+		if (line == null)
+		{
+			file.Close ();
+			return null;
+		}
+		if (line != "TIN")
+		{
+			file.Close();
+			return null;
+		}
+		line = file.ReadLine ();
+		if (line == null)
+		{
+			file.Close();
+			return null;
+		}
+		if (line != "BEGT")
+		{
+			file.Close();
+			return null;
+		}
+		line = file.ReadLine ();
+		if (line == null)
+		{
+			file.Close();
+			return null;
+		}
+		if (!line.Contains("TNAM"))
+		{
+			file.Close();
+			return null;
+		}
+		int length = line.Length - 5;
+		meshName = line.Substring (5,length);
+		line = file.ReadLine ();
+		if (line == null)
+		{
+			file.Close();
+			return null;
+		}
+		if (!line.Contains("TCOL"))
+		{
+			file.Close();
+			return null;
+		}
+		length = line.Length - 5;
+		line = line.Substring (5, length);
+		string[] tempStringArray = line.Split (' ');
+		int tempInt;
+		for (int i = 0; i < tempStringArray.Length; i++) {
+			int.TryParse(tempStringArray[i], out tempInt);
+			color[i] = tempInt;
+		}
+		line = file.ReadLine ();
+		if (line == null)
+		{
+			file.Close();
+			return null;
+		}
+		if (!line.Contains("VERT"))
+		{
+			file.Close();
+			return null;
+		}
+		float tempx, tempy, tempz;
+		length = line.Length - 5;
+		line = line.Substring (5,length);
+		int.TryParse (line, out tempInt);
+		numVertices = tempInt;
+		vertices = new Vector3[numVertices];
+		Vector3 tempVector;
+		for (int i = 0; i < numVertices; i++) {
+			line = file.ReadLine ();
+			if (line == null)
+			{
+				file.Close();
+				return null;
+			}
+			tempStringArray = line.Split (' ');
+			float.TryParse (tempStringArray[0],out tempx);
+			float.TryParse (tempStringArray[1],out tempy);
+			float.TryParse (tempStringArray[2],out tempz);
+			tempVector = new Vector3(tempx,tempy,tempz);
+			vertices[i] = tempVector;
+		}
+		line = file.ReadLine ();
+		if (line == null)
+		{
+			file.Close();
+			return null;
+		}
+		if (!line.Contains("TRI"))
+		{
+			file.Close();
+			return null;
+		}
+		length = line.Length - 4;
+		line = line.Substring (4,length);
+		int.TryParse (line, out tempInt);
+		numTriangles = tempInt;
+		int tempXInt, tempYInt, tempZInt;
+		triangles = new int[numTriangles*3];
+		for (int i = 0; i < numTriangles; i++) {
+			line = file.ReadLine ();
+			if (line == null)
+			{
+				file.Close();
+				return null;
+			}
+			tempStringArray = line.Split (' ');
+			int.TryParse (tempStringArray[0],out tempXInt);
+			int.TryParse (tempStringArray[1],out tempYInt);
+			int.TryParse (tempStringArray[2],out tempZInt);
+			triangles[i*3] = tempXInt-1;
+			triangles[i*3 + 1] = tempYInt-1;
+			triangles[i*3 + 2] = tempZInt-1;
+		}
+		file.Close();
+		
+		Mesh mesh = new Mesh();
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		mesh.RecalculateNormals ();
+		//Vector2[] UV = new Vector2[] {new Vector2(0,256),new Vector2(256,256),new Vector2(256,0),new Vector2(0,0)};
+		//mesh.uv = UV;
+		return mesh;
+	}
+
+	void Start() {
+		Mesh stuff = MeshSetup("example.tin");
+		gameObject.AddComponent<MeshFilter>().mesh = stuff;
+		gameObject.AddComponent<MeshRenderer> ();
+		gameObject.AddComponent<Rigidbody> ();
+		gameObject.GetComponent<Rigidbody> ().isKinematic = true;
+		gameObject.GetComponent<Rigidbody> ().useGravity = false;
+		gameObject.GetComponent<Renderer>().material = mat;
+		gameObject.AddComponent<MeshCollider> ();
+		gameObject.GetComponent<MeshCollider> ().convex = true;
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # WatershedSimulator
+
+Guide:
+	Install Unity 5 Personal from http://unity3d.com/get-unity
+
+	Open Unity
+	Open /Assets/BaseScene.unity either by double clicking in Explorer / Fider or loading it from the "Open Scene" menu in Unity
+	Press the "Run" triangle (play button looking thingy) at the top center of the Unity scene window
+	Press the "Pause" button next to the "Run" triangle to inspect the scene. Mouse wheel will move the camera in and out, and holding "Alt" or "Command" will cause a click and drag to pan the camera
+
+	The current functionality procedurally creates a mesh from a *.TIN file (A common GIS file which is composed of triangles creating a surface - see http://www.xmswiki.com/wiki/TIN_Files for a breakdown of the file type). It applies a rigidbody, a texture/material, a non-kinematic physics model to the mesh and then drops a (currently, non procedural) ball on the mesh. 
+
+Our final project would include the following:
+	A GUI that allows the user to select certain parameters, as well as input a raster / TIN file to be created
+	Create a TIN from an input raster (This could be omitted, it is the hardest part of the project)
+	Create a terrain mesh from a TIN and load it to the scene
+	Procedurally spawn spheres and drop them on the mesh
+	A camera and lighting model that has nice shading
+
+Reach goals:
+	Allow the user to dynamically change where the spheres spawn (i.e. the spheres spawn from a 2d grid that is set above the terrain - let them define the center, length, and width of the grid, as well as the rotation of it)
+	Allow the user camera control so they can navigate the scene
+	Allow the user to set points where they can measure runoff, to get an idea of the percent of dropped water that goes through a certain section of the pourshed
+
+Cool stuff this project includes:
+	We can implement a simple lighting model and any type of static or dynamic camera we want
+	We can apply textures to the environment based on height 
+	It looks pretty
+	It is a surprisingly useful tool for Geographers
+	If we create a TIN from a raster, that would be a super cool mathy thing to do
+	Determining how many spheres we can render at once is a ridiculously interesting computational problem
+

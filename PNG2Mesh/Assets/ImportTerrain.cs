a0b8fd80ef348e574d9c0e5495993e022683aedb
Max Kohl <maxhkohl@github.com>
2015-11-15 21:03:52 -0800

    Testing procedural surface

---
+++ PNG2Mesh/Assets/ImportTerrain.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImportTerrain : MonoBehaviour {
+
+	public GameObject emptyPrefabWithMeshRenderer;
+	public GameObject surface;
+	public string filepath;
+	public Material mat;
+		
+	// Use this for initialization
+	void Start () {
+		Mesh importedMesh = GetComponent<ObjImporter>().ImportFile(filepath);
+		//surface = Instantiate(emptyPrefabWithMeshRenderer,transform.position,transform.rotation);
+		//surface.GetComponent(MeshFilter).mesh = importedMesh;
+		surface.AddComponent<MeshFilter>();
+		surface.GetComponent<MeshFilter>().mesh = importedMesh;
+		surface.AddComponent<MeshRenderer>().material = mat;
+	}
+		
+	// Update is called once per frame
+	void Update () {
+			
+	}
+}
